712c1369738b51e39e000903ddd16a28cb348661
Ho Doan <doanho.it.dev@gmail.com>
2026-01-03 18:26:00 +0700

    v1.0.3

--- Formula/my_nextjs_cli.rb
+++ Formula/my_nextjs_cli.rb
@@ -1,15 +1,15 @@
-class my_nextjs_cli < Formula
-    desc "My Next.js CLI"
-    homepage "https://github.com/ho-doan/homebrew-my_nextjs_cli"
-    url "https://github.com/ho-doan/homebrew-my_nextjs_cli/releases/download/v1.0.3/my_nextjs_cli-1.0.3-macos-arm64.tar.gz"
-    sha256 "e2c8e3756ce203545c7cc72326feb586076e0a6c6fcab24beccd5616f23100c0"
-    version "1.0.3"
-  
-    def install
-      bin.install "my_nextjs_cli"
-    end
-  
-    test do
-      system "#{bin}/my_nextjs_cli"
-    end
+class MyNextjsCli < Formula
+  desc "My Next.js CLI"
+  homepage "https://github.com/ho-doan/homebrew-my_nextjs_cli"
+  url "https://github.com/ho-doan/homebrew-my_nextjs_cli/releases/download/v1.0.3/my_nextjs_cli-1.0.3-macos-arm64.tar.gz"
+  sha256 "e2c8e3756ce203545c7cc72326feb586076e0a6c6fcab24beccd5616f23100c0"
+  version "1.0.3"
+
+  def install
+    bin.install "my_nextjs_cli"
   end
+
+  test do
+    system "#{bin}/my_nextjs_cli"
+  end
+end
